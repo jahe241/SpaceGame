91e6b6a1efb47b6ea0d120c55fd413b5bd186fd1
mb132 <m.bergmann@pm.me>
2024-03-14 13:46:37 +0100

    Set App to always-fullscreen

--- app/src/main/java/com/spacegame/MainActivity.java
+++ app/src/main/java/com/spacegame/MainActivity.java
@@ -1,5 +1,9 @@
 package com.spacegame;
 
+import android.os.Build;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,7 +12,35 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    setFullscreen();
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    setFullscreen();
+  }
+
+  private void setFullscreen() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      // For API level 30 and above
+      WindowInsetsController controller = getWindow().getDecorView().getWindowInsetsController();
+      if (controller != null) {
+        controller.hide(WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
+        controller.setSystemBarsBehavior(
+            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+      }
+    } else {
+      // For API level 16 to 29 Not tested, but should work.
+      getWindow().getDecorView().setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
   }
 }
