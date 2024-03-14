48a77b862ebb0c939e00ff5504c8fe83754036ff
mb132 <m.bergmann@pm.me>
2024-03-14 16:30:55 +0100

    refactored setFullscreen into util class

--- app/src/main/java/com/spacegame/GameActivity.java
+++ app/src/main/java/com/spacegame/GameActivity.java
@@ -1,14 +1,11 @@
 package com.spacegame;
 
+import static com.spacegame.utils.WindowManager.setFullscreen;
+
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 
-import android.view.WindowManager;
 import com.spacegame.core.SpaceRenderer;
 
 
@@ -16,7 +13,7 @@ public class GameActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    setFullscreen(getWindow().getDecorView());
+    setFullscreen(getWindow());
     super.onCreate(savedInstanceState);
     GLSurfaceView glSurfaceView = new GLSurfaceView(this);
     glSurfaceView.setEGLContextClientVersion(2);
@@ -27,33 +24,8 @@ public class GameActivity extends Activity {
   @Override
   protected void onResume() {
     super.onResume();
-    setFullscreen(getWindow().getDecorView());
+    setFullscreen(getWindow());
   }
 
-  private void setFullscreen(View view) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-      // For API level 30 and above
-      WindowInsetsController controller = view.getWindowInsetsController();
-      if (controller != null) {
-        controller.hide(
-            WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
-        controller.setSystemBarsBehavior(
-            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-      }
-      getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-      // We need this, for the SurfaceView to be fullscreen too.
-      getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-          WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-    } else {
-      // For API level 16 to 29
-      view.setSystemUiVisibility(
-          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-              | View.SYSTEM_UI_FLAG_FULLSCREEN
-              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
-  }
 
 }
---
+++ app/src/main/java/com/spacegame/utils/WindowManager.java
@@ -0,0 +1,35 @@
+package com.spacegame.utils;
+
+import android.os.Build;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
+public class WindowManager {
+
+  public static void setFullscreen(android.view.Window window) {
+    var view = window.getDecorView();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      // For API level 30 and above
+      WindowInsetsController controller = view.getWindowInsetsController();
+      if (controller != null) {
+        controller.hide(
+            WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
+        controller.setSystemBarsBehavior(
+            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+      }
+      // We need this, for the SurfaceView to be fullscreen too.
+      window.setFlags(android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+          android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    } else {
+      // For API level 16 to 29
+      view.setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+  }
+}
