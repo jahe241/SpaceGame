b9454b99ba6b25bf09c4c1d57065515fa32e6d1d
mb132@outlook.de <m.bergmann@pm.me>
2024-03-15 21:14:24 +0100

    removed useless imports (testing git config)

--- app/src/main/java/com/spacegame/GameActivity.java
+++ app/src/main/java/com/spacegame/GameActivity.java
@@ -7,47 +7,45 @@ import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class GameActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setFullscreen();
-        super.onCreate(savedInstanceState);
-        GLSurfaceView glSurfaceView = new GLSurfaceView(this);
-        glSurfaceView.setEGLContextClientVersion(2);
-        setContentView(glSurfaceView);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    setFullscreen();
+    super.onCreate(savedInstanceState);
+    GLSurfaceView glSurfaceView = new GLSurfaceView(this);
+    glSurfaceView.setEGLContextClientVersion(2);
+    setContentView(glSurfaceView);
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setFullscreen();
-    }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    setFullscreen();
+  }
 
-    private void setFullscreen() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // For API level 30 and above
-            WindowInsetsController controller = getWindow().getDecorView().getWindowInsetsController();
-            if (controller != null) {
-                controller.hide(WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
-                controller.setSystemBarsBehavior(
-                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-            }
-        } else {
-            // For API level 16 to 29 Not tested, but should work.
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
+  private void setFullscreen() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      // For API level 30 and above
+      WindowInsetsController controller = getWindow().getDecorView().getWindowInsetsController();
+      if (controller != null) {
+        controller.hide(WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
+        controller.setSystemBarsBehavior(
+            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+      }
+    } else {
+      // For API level 16 to 29 Not tested, but should work.
+      getWindow()
+          .getDecorView()
+          .setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
+  }
 }
