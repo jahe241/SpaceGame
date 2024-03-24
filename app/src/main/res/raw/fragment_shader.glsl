daa3e43f981bd5818caf49bab27fd5d1c76c1454
mb132 <m.b132@outlook.de>
2024-03-24 23:27:55 +0100

    fixed ColorEntity not rendering

--- app/src/main/res/raw/fragment_shader.glsl
+++ app/src/main/res/raw/fragment_shader.glsl
@@ -11,14 +11,19 @@ out vec4 outColor;
 
 void main() {
     vec4 texColor = texture(u_Texture, v_TexCoord);
-    if (texColor.a < 0.1) {
+
+    if (v_Flag != 2.0 && texColor.a < 0.1) {
         discard;
+
     } else if (v_Flag == 0.0) {
         outColor = texColor;
+
     } else if (v_Flag == 1.0) {
         outColor = texColor * v_Color;
+
     } else if (v_Flag == 2.0) {
         outColor = v_Color;
+
     } else {
         outColor = texColor;
     }
