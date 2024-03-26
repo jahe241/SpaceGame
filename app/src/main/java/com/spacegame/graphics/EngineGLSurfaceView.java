a750838ea27ae5c54b1bdb49bf1e5802df5fec05
mb132 <m.b132@outlook.de>
2024-03-26 18:52:51 +0100

    moved filtering process to

--- app/src/main/java/com/spacegame/graphics/EngineGLSurfaceView.java
+++ app/src/main/java/com/spacegame/graphics/EngineGLSurfaceView.java
@@ -6,24 +6,31 @@ import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
 import com.spacegame.core.Game;
 import com.spacegame.core.GameInterface;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class EngineGLSurfaceView extends GLSurfaceView {
 
   private final Game game;
   private final GameInterface gameInterface;
-
   private final EngineRenderer renderer;
+
+  // The size.x and size.y fields now contain the width and height of the display in pixels
+  int width;
+  int height;
 
   public EngineGLSurfaceView(Context context) {
     super(context);
     this.setFocusableInTouchMode(true);
     this.requestFocus();
-
-    int screenWidth = context.getResources().getDisplayMetrics().widthPixels;
-    int screenHeight = context.getResources().getDisplayMetrics().heightPixels;
+    // Get the screen width and height
+    //    int screenWidth = context.getResources().getDisplayMetrics().widthPixels;
+    //    int screenHeight = context.getResources().getDisplayMetrics().heightPixels;
 
     // Initialize the game and start it
-    this.game = new Game(screenHeight, screenWidth);
+    getScreenSize(context);
+    this.game = new Game(this.height, this.width);
 
     // Initialize the renderer and set the OpenGL version to 3.0
     this.gameInterface = new GameInterface(context, game);
@@ -40,5 +47,15 @@ public class EngineGLSurfaceView extends GLSurfaceView {
   public boolean onTouchEvent(MotionEvent event) {
     gameInterface.receiveTouchEvent(event);
     return true;
+  }
+
+  private void getScreenSize(Context context) {
+    WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+    Display display = wm.getDefaultDisplay();
+    Point size = new Point();
+    display.getRealSize(size); // getSize() does not include the menu bar
+    width = size.x;
+    height = size.y;
+    size = null;
   }
 }
