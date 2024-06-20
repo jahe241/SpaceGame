48370884d79a63421daa6b75050f61abd658d642
Maximilian Bergmann <994063+mb132@users.noreply.github.com>
2024-06-20 15:36:23 +0200

    Added Background Assets

--- app/src/main/java/com/spacegame/utils/Constants.java
+++ app/src/main/java/com/spacegame/utils/Constants.java
@@ -7,6 +7,37 @@ public class Constants {
   public static String FONT_PREFIX = "joystix_";
 
   public static String PLANETS = "background_planet_";
+  public static int NUM_BACKGROUND_ASSETS = 28;
+  public static String[] BACKGROUND_ASSETS = {
+    "background_planet_001",
+    "background_planet_002",
+    "background_planet_003",
+    "background_planet_004",
+    "background_planet_005",
+    "background_planet_006",
+    "background_planet_007",
+    "background_planet_008",
+    "background_planet_009",
+    "background_planet_010",
+    "background_planet_011",
+    "background_planet_012",
+    "background_planet_013",
+    "background_planet_014",
+    "background_planet_015",
+    "background_planet_016",
+    "background_planet_017",
+    "background_planet_018",
+    "background_planet_019",
+    "background_planet_020",
+    "background_planet_021",
+    "background_planet_022",
+    "background_planet_023",
+    "background_planet_024",
+    "background_planet_025",
+    "background_planet_026",
+    "background_planet_027",
+    "background_planet_028"
+  };
 
   public static String[] GAMEPAD = {
     "touch_input_2d_white", "touch_input_cursor_white"
